47ed09e1e257bb9b0d6f91f2999419ff405677f1
ManuelPerezF <A01741957@tec.mx>
2025-03-03 07:28:31 -0600

    Modificacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,8 @@ export default function Page() {
   const [cities, setCities] = useState([])
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-top min-h-screen py-2">
+      <h1 className="text-4xl font-bold text-center py-4">Aplicacion Clima</h1>
       <SearchBar onSearch={(city) => setCities([])} />
     
     </div>
