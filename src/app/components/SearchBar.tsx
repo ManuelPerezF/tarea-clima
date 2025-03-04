ca7c5800a730c7801512144ed4c96b249223f516
ManuelPerezF <A01741957@tec.mx>
2025-03-03 19:35:39 -0600

    SearchBar

---
+++ src/app/components/SearchBar.tsx
@@ -0,0 +1,36 @@
+'use client';
+import React, { useState } from "react";
+
+interface SearchBarProps {
+  onSearch: (city: string) => void;
+}
+
+export default function SearchBar({ onSearch }: SearchBarProps) {
+  const [city, setCity] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (city.trim()) {
+        onSearch(city);
+        setCity('');    
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex justify-center items-center">
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Buscar ciudad..."
+        className="border border-gray-300 rounded-l px-4 py-2 focus:outline-none w-64 text-black"
+      />
+      <button   
+        type="submit"
+        className="bg-blue-500 text-white px-4 py-2 rounded-r hover:bg-blue-600"
+      >
+        Buscar
+      </button>
+    </form>
+  );
+}
